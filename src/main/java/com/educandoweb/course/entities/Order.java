4f7c7d56bd009981c6220035a41f2fd08cde31a3
junior-menezes <casagrandejuniorm@gmail.com>
2020-09-15 00:03:04 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -11,24 +11,26 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tb_order")
-public class Order  implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-		
+
 	public Order() {
 	}
-	
-	
 
 	public Order(Long id, Instant moment, User client) {
 		super();
@@ -36,8 +38,6 @@ public class Order  implements Serializable{
 		this.moment = moment;
 		this.client = client;
 	}
-
-	
 
 	public Long getId() {
 		return id;
@@ -63,8 +63,6 @@ public class Order  implements Serializable{
 		this.client = client;
 	}
 
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -72,8 +70,6 @@ public class Order  implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -91,7 +87,5 @@ public class Order  implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable{
@@ -24,6 +26,7 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@JsonIgnore // é preciso colocar essa anotation em uma associação quando temos uma associação de mão dupla strange key dos dois lados.
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
